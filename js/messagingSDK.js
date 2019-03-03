9dfbe06dcf4f8941ab43f2d58e3aff62a28de116
Eden Kupermintz <edenk@liveperson.com>
2019-03-03 11:12:03 +0200

    casey is presentable

--- js/messagingSDK.js
+++ js/messagingSDK.js
@@ -7,24 +7,25 @@ windowKit.connect();
 
 windowKit.onAgentTextEvent(function(text) {
 	$('#caseyContainer').append('<div class="caseyText">' + text + '</div>');
+	var botTexts = document.getElementsByClassName('caseyText');
+	var latestText = botTexts[botTexts.length - 1]
+	$('body, html').animate({ scrollTop: $(latestText).offset().top }, 1000);
 	console.log('Agent: ' + text);
 });
 
 windowKit.onAgentRichContentEvent(function(content) {
   var structuredText = JsonPollock.render(content);
 	$('#caseyContainer').append(structuredText);
+	var botTextsSC = document.getElementsByClassName('lp-json-pollock');
+	var latestSC = botTextsSC[botTextsSC.length - 1];
+	$('body, html').animate({ scrollTop: $(latestSC).offset().top }, 1000);
 	console.log('Agent: ', structuredText);
 	$('.lp-json-pollock-element-button').on('click', function () {
 		var scText = $(this).text();
 		windowKit.sendMessage(scText);
+		$('#caseyContainer').append('<div class="consumerText">' + scText + '</div>');
+		var consumerTexts = document.getElementsByClassName('consumerText');
+		var latestConsumerText = consumerTexts[consumerTexts.length - 1];
+		$('body, html').animate({ scrollTop: $(latestConsumerText).offset().top }, 1000);
 	});
-});
-
-windowKit.onVisitorTextEvent(function(text){
-    $('#caseyContainer').append('<div class="consumerText">' + text + '</div>');
-    console.log('Consumer');
-});
-
-$(document).ready(function () {
-
 });
--- pages/documents/Getting Started/casey.md
+++ pages/documents/Getting Started/casey.md
@@ -1,16 +1,15 @@
 ---
-pagename: Casey Lives Here
+pagename: Casey
 categoryName: Getting started
 subCategoryName: ''
 indicator: both
-subtitle: Casey's future home
+subtitle:
 level3: ''
-permalink: getting-started-livepersons-casey.html
-isTutorial: true
+permalink: casey.html
+isTutorial: false
 isNew: false
 date: 2019-01-23 13:04:36 +0000
 layout: bot
-
 ---
 
 <div id="caseyContainer">
