0cd47314fc3957f4bdad37ba1a5296daf3c4343f
Eden Kupermintz <edenk@liveperson.com>
2019-03-28 13:23:38 +0200

    closed casey for non homepage

--- js/scripts.js
+++ js/scripts.js
@@ -31,12 +31,12 @@ function navigateContent(url) {
 			var $content = $('#defaultcontent');
 			var $titlecontainer = $('.documenttitle');
 			var $breadcrumbs = $('.breadcrumbs');
-			var $header = $('#defaultheader');
+			var $botImage = $('.caseyPortrait');
 			//exchange the content of those parts with the new parts loaded via ajax
 			$breadcrumbs.html($newData.find('.breadcrumbs').html());
 			$titlecontainer.html($newData.find('.documenttitle').html());
 			$content.html($newData.find('#defaultcontent').html());
-			$header.html($newData.find('#defaultheader').html());
+			$('.caseyPortrait').attr('src', 'img/closed-casey-header.svg');
 			//add anchor links to all h3 titles. See respective functions below for what they do.
 			if ($titlecontainer.html().indexOf('Welcome' == -1)) {
 				$('#documenttitlecontainer').removeClass('botTitle')
