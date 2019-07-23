93f57bd1f08e2cf7e0effaaf1f2f7dcb489fb853
Eden Kupermintz <edenk@liveperson.com>
2019-07-23 17:39:39 +0300

    minimizing a bunch of images + lfs

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,6 @@ var browserSync = require('browser-sync').create();
 
 // Task for building blog when something changed:
 gulp.task('build', shell.task(['bundle exec jekyll serve --incremental']));
-// If you don't use bundle:
-// gulp.task('build', shell.task(['jekyll serve']));
-// If you use  Windows Subsystem for Linux (thanks @SamuliAlajarvela):
-// gulp.task('build', shell.task(['bundle exec jekyll serve --force_polling']));
-
 // Task for serving blog with Browsersync
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}});
