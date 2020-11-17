d139c26780d1fd0685c47a605c72cba7d9f5b74d
Andrew Cosmic <acosmic@liveperson.com>
2020-11-17 15:29:13 -0800

    created gulp to make json, rendering using jquery

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var browserSync = require('browser-sync').create();
 
 // Task for building blog when something changed:
 gulp.task('build', shell.task(['bundle exec jekyll serve --incremental']));
+gulp.task('build', shell.task(['./script/convertYaml.sh']));
 // If you don't use bundle:
 // gulp.task('build', shell.task(['jekyll serve']));
 // If you use  Windows Subsystem for Linux (thanks @SamuliAlajarvela):
--- js/load-metric-table.js
+++ js/load-metric-table.js
@@ -1,22 +1,39 @@
 (function () {
-  metricData =[];
-  try {    
-    metricData.map((metricitem)=>{
-      let { Metric, analysisType, Channel, Description, Dashboard, filteredBy, formulaOptional} = metricitem;
-      $(".metric-table").append(`
-      <tr style="display:none">
-        <td class="metric">${Metric}</td>
-        <td class="analysis">${ analysisType }</td>
-        <td class="channel">${ Channel }</td>
-        <td class="description">${ Description}</td>
-        <td class="dashboard">${Dashboard}</td>
-        <td class="filtered">${filteredBy}</td>
-        <td class="formula">${formulaOptional}</td>
-      </tr>
-    `);
-    });
+  var staticUrl = "/data/reportbuild.json"
+  fetch(staticUrl)
+    .then(response => response.json())
+    .then(metricData => {
+      metricData.map((metricitem) => {
+        let { Metric, analysisType, Channel, Description, Dashboard, filteredBy, formulaOptional } = metricitem;
+        jQuery(".metric-table").append(`
+          <tr>
+            <td class="metric">${Metric}</td>
+            <td class="analysis">${analysisType}</td>
+            <td class="channel">${Channel}</td>
+            <td class="description">${Description}</td>
+            <td class="dashboard">${Dashboard}</td>
+            <td class="filtered">${filteredBy}</td>
+            <td class="formula">${formulaOptional}</td>
+          </tr>
+        `);
+      });
+    })
+})();
+
+
+    // metricData.map((metricitem)=>{
+    //   let { Metric, analysisType, Channel, Description, Dashboard, filteredBy, formulaOptional} = metricitem;
+    //   $(".metric-table").append(`
+    //   <tr style="display:none">
+    //     <td class="metric">${Metric}</td>
+    //     <td class="analysis">${ analysisType }</td>
+    //     <td class="channel">${ Channel }</td>
+    //     <td class="description">${ Description}</td>
+    //     <td class="dashboard">${Dashboard}</td>
+    //     <td class="filtered">${filteredBy}</td>
+    //     <td class="formula">${formulaOptional}</td>
+    //   </tr>
+    // `);
+    // });
    
-  } catch (e) {
-    console.log(e);
-  }
-}());
+// }());
