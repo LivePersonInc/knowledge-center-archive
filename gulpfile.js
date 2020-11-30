3083a20d09ad08db9f0cd515f584d7236b089204
Andrew Cosmic <acosmic@liveperson.com>
2020-11-30 13:25:59 -0800

    reverted gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,8 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
-// Added converstion of yaml to json in the build task
 // Task for building blog when something changed:
-gulp.task('build', shell.task(['./scripts/convertYaml.sh', 'bundle exec jekyll serve --incremental']));
+gulp.task('build', shell.task(['bundle exec jekyll serve --incremental']));
 // If you don't use bundle:
 // gulp.task('build', shell.task(['jekyll serve']));
 // If you use  Windows Subsystem for Linux (thanks @SamuliAlajarvela):
@@ -17,4 +16,4 @@ gulp.task('serve', function () {
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('build', 'serve'));
+gulp.task('default', gulp.parallel('build', 'serve'));
