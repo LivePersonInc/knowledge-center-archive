2715112bfc4b7d70aaf538db27457b515732b24f
Andrew Cosmic <acosmic@liveperson.com>
2020-11-30 13:32:05 -0800

    cleaned up load metric table

--- js/load-metric-table.js
+++ js/load-metric-table.js
@@ -19,21 +19,3 @@
       });
     })
 })();
-
-
-    // metricData.map((metricitem)=>{
-    //   let { Metric, analysisType, Channel, Description, Dashboard, filteredBy, formulaOptional} = metricitem;
-    //   $(".metric-table").append(`
-    //   <tr style="display:none">
-    //     <td class="metric">${Metric}</td>
-    //     <td class="analysis">${ analysisType }</td>
-    //     <td class="channel">${ Channel }</td>
-    //     <td class="description">${ Description}</td>
-    //     <td class="dashboard">${Dashboard}</td>
-    //     <td class="filtered">${filteredBy}</td>
-    //     <td class="formula">${formulaOptional}</td>
-    //   </tr>
-    // `);
-    // });
-   
-// }());
