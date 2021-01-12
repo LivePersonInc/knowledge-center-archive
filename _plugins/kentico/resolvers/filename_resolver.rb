8325a4aeca4bf7aea5078c8ee9a4384c904d2048
Andrew Cosmic <acosmic@liveperson.com>
2021-01-12 14:52:03 -0800

    working templates that need clean up

---
+++ _plugins/kentico/resolvers/filename_resolver.rb
@@ -0,0 +1,21 @@
+class FilenameResolver
+  def resolve(item)
+    sitemap_arr=item.elements.sitemap.value
+    folder_slug=''
+    #  site made has the data start bottom and end top
+    sitemap_arr.reverse_each{ |i| folder_slug+=i.codename+"-" }
+    url_slug = get_url_slug(item)
+    
+    "#{folder_slug}#{url_slug.value}"
+  end
+
+  private
+
+  def get_url_slug(item)
+    item.elements.each_pair { |_codename, element| return element if slug?(element) }
+  end
+
+  def slug?(element)
+    element.type == Kentico::Kontent::Constants::ItemElementType::URL_SLUG
+  end
+end
